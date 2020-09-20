bc92e4616fd9ecc90991e4b38e2ed598852153bd
davejj1530 <davejjceriola@gmail.com>
2020-09-20 17:27:22 +0800

    Updates

---
+++ precache-manifest.c4cc17b5290782ef43860e330abfddd8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e50eda85f88592ba91eca2e67b9cd06d",
+    "url": "/online-shop/index.html"
+  },
+  {
+    "revision": "653c626b055b4348d73b",
+    "url": "/online-shop/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "f11802a56e206db68dc5",
+    "url": "/online-shop/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "653c626b055b4348d73b",
+    "url": "/online-shop/static/js/2.4ebd6beb.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/online-shop/static/js/2.4ebd6beb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f11802a56e206db68dc5",
+    "url": "/online-shop/static/js/main.87eb23d7.chunk.js"
+  },
+  {
+    "revision": "f5b22b80dcdf99f7e646",
+    "url": "/online-shop/static/js/runtime-main.2386cdfb.js"
+  }
+]);
