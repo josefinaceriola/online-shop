a76b5f2c90f0701d58a587f9261ae714bd2b733c
davejj1530 <davejjceriola@gmail.com>
2020-09-20 17:57:31 +0800

    fb messenger

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import Home from './pages/Home';
 import MessengerCustomerChat from 'react-messenger-customer-chat';
-
+//token EAAhtQCA7ZAykBACPo6N0xbBLwN8ylZAF2emowXQX7d14iwSZCREGUFd7wAqR9nRdTznx7ZAliafMLRl68uoZC3mH45Hh411ZCckZArStBw0PUzj7noLsUYptcZAZAZBUawsf8Th7zZCY2pbOFF1GIBgnbqxkRZCPUpcZCpQOnapQVPXzfxKfbAOtZC83YUCkRG3JSIwU8ZD
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className='App'>
       <Home />
-      {/* <MessengerCustomerChat
-    pageId="119546696561154
-    "
-    appId="2371921999783721"
-   
-  /> */}
+      <MessengerCustomerChat
+        pageId='119546696561154'
+        appId='2371921999783721'
+      />
     </div>
   );
 }
